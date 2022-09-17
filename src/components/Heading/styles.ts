f70ac6738d965adbe29afa289e416d5bdcd44d5b
Vitor Santana <vitor@buildbox.com.br>
2022-09-17 13:04:45 -0300

    style: :lipstick: style change

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -10,6 +10,7 @@ export const styles = StyleSheet.create({
     color: THEME.COLORS.TEXT,
     fontSize: THEME.FONT_SIZE.LG,
     fontFamily: THEME.FONT_FAMILY.BLACK,
+    marginBottom: 6,
   },
   subtitle: {
     color: THEME.COLORS.CAPTION_400,
